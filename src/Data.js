322c5ffdc6c77c5c2708b492c13f7852d9ee788c
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2023-02-24 11:01:00 +0100

    finish

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+let shopItemsData=[
+    {
+        id:"t1289",
+        name:"Casual shitrs",
+        price:45,
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img:"images/T5.jpg"
+     },{
+        id:"t1389",
+        name:"Casual shitrs",
+        price:35,
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img:"images/T4.jpg"
+     },{
+        id:"t1489",
+        name:"Woman shitrs",
+        price:15,
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img:"images/T3.jpg"
+     },{
+        id:"t1589",
+        name:"Men shitrs",
+        price:25,
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img:"images/T2.jpg"
+     },{
+      id:"t1589",
+      name:"Men shitrs",
+      price:25,
+      desc:"Lorem ipsum dolor, sit amet consectetur adipisicing.",
+      img:"images/T2.jpg"
+   }]
